536b0745998b35c8fbb16008c616952099a565c7
evelyn <liuevelyn@outlook.com>
2017-12-23 02:43:01 +0000

    insert Hashtag & Bullet classes

--- tables.py
+++ tables.py
@@ -20,7 +20,7 @@ class Friend(db.Model):
     __tablename__ = "friends"
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    name = db.Column(db.Text)
+    name = db.Column(db.Text, nullable = False)
     interests = db.Column(db.Text, default="")
     dislikes = db.Column(db.Text, default="")
     quotes = db.Column(db.Text, default="")
@@ -35,6 +35,7 @@ class Friend(db.Model):
         self.user_id = user_id
         self.name = name
 
+#Hastags' IDs
 class Hashtag(db.Model):
 
     __tablename__ = "hashtags"
@@ -44,6 +45,20 @@ class Hashtag(db.Model):
     def __init__(self, hashtag):
         self.hashtag = hashtag
 
+#Bullet points
+class Bullet(db.Model):
 
+    __tablename__ = "bullets"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    hashtag_id = db.Column(db.Integer, db.ForeignKey("hashtags.id"), nullable = False)
+    friend_id = db.Column(db.Integer, db.ForeignKey("friends.id"), nullable = False)
+    category = db.Column(db.Text, default="", nullable = False)
+    content = db.Column(db.Text, default="", nullable = False)
+
+    def __init__(self, hashtag_id, friend_id, category, content):
+        self.hashtag_id = hashtag_id
+        self.friend_id = friend_id
+        self.category = category
+        self.content = content
 
 db.create_all()
